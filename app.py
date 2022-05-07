ed985c26fa1d121a7175818da615306d18890508
Maxchh1402 <102831688+Maxchh1402@users.noreply.github.com>
2022-05-07 08:34:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.markdown("<h1 style='text-align: center; color:lightblue;'>Ecuaciones de Maxwell</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color:lightblue;'>Ecuaciones de Maxwell</h1>")
 st.button('click aqui')
 st.balloons()
 st.title('Ley de Gauss')
