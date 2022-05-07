024f1c40f747b4a89795607f934ad1dfa366c1a4
Maxchh1402 <102831688+Maxchh1402@users.noreply.github.com>
2022-05-07 08:29:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.markdown("<h1 style='text-align: center; color: red;'>Some title</h1>", unsafe_allow_html=True)
 st.title('Ecuaciones de Maxwell')
 st.button('click aqui')
 st.balloons()
