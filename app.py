33c67ad78626e0ed2568688a3bf7fa182984c8bc
Maxchh1402 <102831688+Maxchh1402@users.noreply.github.com>
2022-05-07 09:28:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ de una manera muy precisa: con el cuadrado de la distancia.""")
 
 
 st.latex(r'''
-    \epsilon + \rho + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \overrightarrow{V}* \overrightarrow{E} \epsilon + \rho + a r^2 + a r^3 + \cdots + a r^{n-1} =
      \sum_{k=0}^{n-1} ar^k =
      a \left(\frac{1-r^{n}}{1-r}\right)
      ''')
