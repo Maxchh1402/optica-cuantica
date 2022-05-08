fd8cdc6abd343174aee4be0b55045aa8ec8dc40d
Maxchh1402 <102831688+Maxchh1402@users.noreply.github.com>
2022-05-07 20:52:04 -0600

    d

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 st.markdown("<h1 style='text-align: center; color:lightblue;'>Ecuaciones de Maxwell</h1>", unsafe_allow_html=True)
 import pandas as pd
-st.markdown("<h1 style='text-align: center; color:green;'>Ley de Gauss</h1>", unsafe_allow_html=True)
-st.title('Ley de Gauss')
+st.markdown("<h1 style='text-align:color:green;'>Ley de Gauss</h1>", unsafe_allow_html=True)
 st.latex(r'''
     \overrightarrow{\nabla}* \overrightarrow{E} = \frac{\rho}{\epsilon_0}   
  ''')
